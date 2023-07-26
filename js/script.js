68caebaed614d9911c005e70252e28e9bbad1e90
rcgabreu <rcga.dev@gmail.com>
2023-07-26 08:05:39 -0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-var swiper = new Swiper(".slide-content", {
+/* CARROSSEL */
+
+  var swiper = new Swiper(".slide-content", {
     slidesPerView: 1,
-    spaceBetween: 8,
+    spaceBetween: 2,
     loop: true,
     pagination: {
       el: ".swiper-pagination",
@@ -11,3 +13,27 @@ var swiper = new Swiper(".slide-content", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+/* AVALIAÇÃO */
+  
+var stars = document.querySelectorAll('.star-icon');
+                  
+document.addEventListener('click', function(e){
+  var classStar = e.target.classList;
+  if(!classStar.contains('ativo')){
+    stars.forEach(function(star){
+      star.classList.remove('ativo');
+    });
+    classStar.add('ativo');
+    console.log(e.target.getAttribute('valor-avaliacao'));
+  }
+});
+
+/* MENU */ 
+function clickMenu(){
+  if (itens.style.display == 'block'){
+    itens.style.display = 'none'
+  } else {
+    itens.style.display = 'block'
+  }
+}
